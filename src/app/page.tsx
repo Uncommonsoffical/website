bedc938d3d371210e24b45e341973d72b288f33d
wwei-github <418047394@qq.com>
2023-11-22 14:58:16 +0800

    feat: remove next-seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import Workstream from "../../components/Home/WorkStream";
 import Collaborator from "../../components/Home/Partner";
 import { useStore } from "@/store";
 import { useEffect } from "react";
-import { NextSeo } from "next-seo";
 
 export default function Home() {
   const isPC = useMediaQuery("(min-width:1200px)");
@@ -28,27 +27,6 @@ export default function Home() {
 
   return (
     <Box>
-      <NextSeo
-        title="Uncommmons"
-        description="Uncommons is a digital commons a digital garden for public goods builders."
-        canonical="https://www.uncommons.cc/"
-        openGraph={{
-          url: "https://www.uncommons.cc/",
-          title: "Uncommmons",
-          description:
-            "Uncommons is a digital commons a digital garden for public goods builders.n",
-          siteName: "Uncommmons",
-          type: "article",
-          article: {
-            tags: ["uncommons", "dao", "lxdao", "plancker"],
-          },
-        }}
-        twitter={{
-          handle: "@Un__commons",
-          site: "@Un__commons",
-          cardType: "summary_large_image",
-        }}
-      />
       <Introduce />
       <Information />
       <ArticleBox />
