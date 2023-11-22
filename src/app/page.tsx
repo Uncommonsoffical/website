4ea70c74fff6ab81a51782823ce92b7b8f3d348b
wwei-github <418047394@qq.com>
2023-11-22 14:16:08 +0800

    feat: add seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Workstream from "../../components/Home/WorkStream";
 import Collaborator from "../../components/Home/Partner";
 import { useStore } from "@/store";
 import { useEffect } from "react";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
   const isPC = useMediaQuery("(min-width:1200px)");
@@ -18,17 +19,36 @@ export default function Home() {
     console.log(isPC, isIpad, isPhone);
     if (isPC) {
       setScreenSize({ isPC, isIpad: false, isPhone: false });
-    }
-    if (isIpad) {
+    } else if (isIpad) {
       setScreenSize({ isIpad, isPC: false, isPhone: false });
-    }
-    if (!isIpad && isPhone) {
+    } else if (!isIpad && isPhone) {
       setScreenSize({ isPhone, isPC: false, isIpad: false });
     }
   }, [isPC, isIpad, isPhone]);
 
   return (
     <Box>
+      <NextSeo
+        title="Uncommmons"
+        description="Uncommons is a digital commons a digital garden for public goods builders."
+        canonical="https://www.uncommons.cc/"
+        openGraph={{
+          url: "https://www.uncommons.cc/",
+          title: "Uncommmons",
+          description:
+            "Uncommons is a digital commons a digital garden for public goods builders.n",
+          siteName: "Uncommmons",
+          type: "article",
+          article: {
+            tags: ["uncommons", "dao", "lxdao", "plancker"],
+          },
+        }}
+        twitter={{
+          handle: "@Un__commons",
+          site: "@Un__commons",
+          cardType: "summary_large_image",
+        }}
+      />
       <Introduce />
       <Information />
       <ArticleBox />
