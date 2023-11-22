89f685b79570644b746d90ac042c8a480498e94d
wwei-github <418047394@qq.com>
2023-11-22 14:49:33 +0800

    feat: change seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,43 @@ import WalletConnect from "../../components/WalletConnect";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "Uncommmons";
+const description =
+  "Uncommons isa digital commons digital garden public goods builders";
+
 export const metadata: Metadata = {
-  title: "Uncommmons",
-  description: "Uncommmons",
+  title,
+  description,
+  generator: "Uncommmons",
+  applicationName: "Uncommmons",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Uncommmons", "dao", "web3", "lxdao"],
+  authors: [{ name: "Uncommmons" }],
+  creator: "Uncommmons",
+  publisher: "Uncommmons",
+
+  openGraph: {
+    title,
+    description,
+    url: "https://uncommons.cc",
+    siteName: "Uncommmons",
+    images: [
+      {
+        url: "https://uncommons.cc/logo.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@Un__commons",
+    images: ["https://uncommons.cc/logo.png"],
+  },
 };
 
 export const viewport: Viewport = {
